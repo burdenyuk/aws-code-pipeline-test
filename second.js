a73e1d4ec96d5d696dc4a6307259534e290c999f
Denys Burdeniuk <burdenyukdenis@gmail.com>
2016-12-29 10:57:11 +0100

    Added console.logs

--- second.js
+++ second.js
@@ -1,4 +1,8 @@
 exports.list = (event, context, callback) => {
+
+    console.log('event: ', event);
+    console.log('context: ', context);
+
     callback(null, {
         statusCode: '200',
         body: 'It is list'
